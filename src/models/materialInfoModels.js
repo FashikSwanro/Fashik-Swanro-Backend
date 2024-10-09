febedc6bc9061bb2e01ef94a7b74b3809db9a1ac
Ansh <anshtandonat@gmail.com>
2024-10-09 21:41:52 +0530

    made changes in material model

--- src/models/materialInfoModels.js
+++ src/models/materialInfoModels.js
@@ -5,7 +5,9 @@ const materialInfoModelSchema = new mongoose.Schema({
     description : {type: String, required: false, default: ""},
     price : {type: String, required: false, default: ""},
     imgUrl : {type: String, required: false, default: ""},
-    type : {type: String, required: false, default: ""} // style , design , color , fabric 
+    type : {type: String, required: false, default: ""} ,// style , design , color , fabric 
+    handwork : {type: String, required: false, default: ""},
+    fabricUsed : {type: String, required: false, default: ""},
 })
 
 const MaterialInfo = mongoose.model("MaterialInfo" , materialInfoModelSchema);
